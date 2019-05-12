6bf1adda9bf7bd59ad4414ae65b9c2db831d4463
Nathan Ellenfield <nathan.ellenfield@gmail.com>
2019-05-12 00:40:08 -0400

    Added (very) basic functional tests

---
+++ IdentityServer4.WsFederation.Tests/Functional/SigninTests.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer4.WsFederation.Server;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.WsFederation.Tests.Functional
+{
+    [TestClass]
+    public class SigninTests
+    {
+        private readonly  HttpClient _client;
+
+        public SigninTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            _client = factory.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task SigninWithNoContextRedirectsToLogin()
+        {
+            var response = await _client.GetAsync("/wsfederation/signin?wa=wsignin1.0&wtrealm=urn:idsrv4:wsfed:sample");
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("/Account/Login", response.RequestMessage.RequestUri.LocalPath);
+            StringAssert.Contains(content, "<h1>Login</h1>");
+        }
+    }
+}
