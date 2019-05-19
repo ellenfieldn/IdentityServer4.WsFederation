1f33ba0ba51e09891db4480407f63a9e7049eed5
Nathan Ellenfield <nathan.ellenfield@gmail.com>
2019-05-19 02:33:45 -0400

    misc. code cleanup

--- src/IdentityServer4.WsFederation/Validation/WsFederationSignoutValidator.cs
+++ src/IdentityServer4.WsFederation/Validation/WsFederationSignoutValidator.cs
@@ -2,6 +2,7 @@
 using IdentityServer4.Stores;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Protocols.WsFederation;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,48 +33,45 @@ namespace IdentityServer4.WsFederation.Validation
             validatedRequest.SessionId = await _userSession.GetSessionIdAsync();
             validatedRequest.ClientIds = await _userSession.GetClientListAsync();
 
-            //Wtrealm is not really part of the standard for signout, but it appears that some implementations include it
+            IEnumerable<string> postLogoutRedirectUris;
+            //Wtrealm is not really part of the standard for signout, but it appears that some implementations include it.
+            //If the wtrealm is specified, respect it. Otherwise, search all of the user's known clients for the wreply.
             if (!string.IsNullOrEmpty(message.Wtrealm))
             {
+                _logger.LogDebug("Wtrealm was specified. Using PostLogoutRedirectUris for only that client.", validatedRequest);
                 var client = await _clients.FindEnabledClientByIdAsync(message.Wtrealm);
-                if (client != null)
+                if (client == null)
                 {
-                    if (message.Wreply == null)
-                    {
-                        validatedRequest.PostLogOutUri = client.PostLogoutRedirectUris.FirstOrDefault();
-                    }
-                    else if (client.PostLogoutRedirectUris.Contains(message.Wreply))
-                    {
-                        validatedRequest.PostLogOutUri = message.Wreply;
-                    }
-                    else
-                    {
-                        _logger.LogError("The passed in redirect url is not valid for the given client.", validatedRequest);
-                        return new WsFederationSignoutValidationResult(validatedRequest, "Invalid redirect uri.", "The passed in redirect url is not valid for the given client.");
-                    }
-                    return new WsFederationSignoutValidationResult(validatedRequest);
+                    _logger.LogError("There is no client configured that matches the wtrealm parameter of the incoming request.", validatedRequest);
+                    return new WsFederationSignoutValidationResult(validatedRequest, "No Client.", "There is no client configured that matches the wtrealm parameter of the incoming request.");
                 }
-            }
-
-            var clients = (await Task.WhenAll(validatedRequest.ClientIds.Select(async c => await _clients.FindEnabledClientByIdAsync(c))))
-                .Where(c => !string.IsNullOrEmpty(c.PostLogoutRedirectUris?.FirstOrDefault()));
-
-            if (string.IsNullOrEmpty(message.Wreply))
-            {
-                _logger.LogInformation("Wreply is missing from the request. Using the defualt wreply.", validatedRequest);
-                validatedRequest.PostLogOutUri = clients.FirstOrDefault().PostLogoutRedirectUris.FirstOrDefault();
-                return new WsFederationSignoutValidationResult(validatedRequest);
+                postLogoutRedirectUris = client.PostLogoutRedirectUris;
             }
             else
             {
-                if (!clients.Any(c => c.PostLogoutRedirectUris.Contains(message.Wreply)))
-                {
-                    _logger.LogError("The passed in redirect url is not valid for the given client.", validatedRequest);
-                    return new WsFederationSignoutValidationResult(validatedRequest, "Invalid redirect uri.", "The passed in redirect url is not valid for the given client.");
-                }
-                validatedRequest.PostLogOutUri = message.Wreply;
-                return new WsFederationSignoutValidationResult(validatedRequest);
+                _logger.LogDebug("Wtrealm was not specified. Using PostLogoutRedirectUris for all clients that the user has authenticated with.", validatedRequest);
+                var tasks = validatedRequest.ClientIds.Select(async c => await _clients.FindEnabledClientByIdAsync(c)).ToList();
+                var clients = await Task.WhenAll(tasks);
+                postLogoutRedirectUris = clients.SelectMany(c => c.PostLogoutRedirectUris).Where(uri => !string.IsNullOrEmpty(uri));
             }
+
+            //This behavior might be odd. If the user is authenticated with multiple clients, it's probably bad to redirect them to a random client....
+            if (string.IsNullOrEmpty(message.Wreply))
+            {
+                _logger.LogInformation("Wreply is missing from the request. Using the defualt wreply.", validatedRequest);
+                validatedRequest.PostLogOutUri = postLogoutRedirectUris.FirstOrDefault();
+            }
+            else if (postLogoutRedirectUris.Contains(message.Wreply))
+            {
+                validatedRequest.PostLogOutUri = message.Wreply;
+            }
+            else
+            {
+                _logger.LogError("The passed in redirect url is not valid for the given client.", validatedRequest);
+                return new WsFederationSignoutValidationResult(validatedRequest, "Invalid redirect uri.", "The passed in redirect url is not valid for the given client.");
+            }
+            _logger.LogTrace("WsFederation signout request validation successful.");
+            return new WsFederationSignoutValidationResult(validatedRequest);
         }
     }
 }
